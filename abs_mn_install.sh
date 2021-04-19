31b5cc582b2b6317949afca70e8ccea91cb4c678
BIllI7676 <puiuflorinel@yahoo.com>
2021-04-19 23:43:40 +0300

    update seed nodes

--- abs_mn_install.sh
+++ abs_mn_install.sh
@@ -137,8 +137,8 @@ if [ ! -d "$abs_path" ]; then
 	{
 	printf "\n#--- basic configuration --- \nrpcuser=abs_mn_user\nrpcpassword=$rpc_pass\nrpcport=18889\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=256\nrpcallowip=127.0.0.1\nexternalip=%s:18888\n" "$ext_ip"
 	printf "\n#--- masternode ---\nmasternode=1\nmasternodeprivkey=%s\n" "$mn_key"
-	printf "\n#--- new nodes ---\naddnode=118.69.72.95:18888\naddnode=80.211.81.251:18888\naddnode=88.198.119.136:18888\naddnode=46.97.97.38:18888\n"
-	printf "addnode=45.77.138.219:18888\naddnode=95.216.209.25:18888\naddnode=116.203.202.68:18888\naddnode=62.121.77.173:18888\n"
+	printf "\n#--- new nodes ---\naddnode=118.69.72.95:18888\naddnode=80.211.81.251:18888\naddnode=88.198.119.136:18888\naddnode=46.97.97.38:18888\naddnode=62.121.77.173:18888\n"
+	printf "addnode=45.77.138.219:18888\naddnode=95.216.209.25:18888\naddnode=116.203.202.68:18888\naddnode=95.217.232.25:18888\naddnode=116.203.255.12:18888\naddnode=168.119.97.188:18888\n"
 	} > "$abs_conf_file"
 
 	printSuccess "...done!"
